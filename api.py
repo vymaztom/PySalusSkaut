92b98a09723e5d41a10925ef96d76f9bb8c2c034
vyma.96@gmail.com <>
2021-07-20 22:46:32 +0200

    add logger

--- api.py
+++ api.py
@@ -11,9 +11,10 @@ from pyit600.gateway_singleton import IT600GatewaySingleton
 from ExelLib_TT import *
 from sendToThingSpeak import ThingSpeakSender
 
-def printTXTfile(fileName):
+def printTXTfile(fileName, file):
 	with open(fileName,"r") as f:
 		print(f.read())
+		file.write(f.read())
 
 
 async def my_climate_callback(device_id):
@@ -40,27 +41,40 @@ async def main():
 	argData = config['DEFAULT']
 	listUID = config['ThingSpeakFields']
 
+	logger = open(config['nameLogFile'],"a")
+
 	# print Header
 	for i in range(79):
 		print("-", end="")
+		logger.write("-")
 	print("-")
-	printTXTfile("banner.txt")
+	logger.write("-" + "\n")
+	printTXTfile("banner.txt", logger)
 	now = datetime.datetime.now()
 	current_time = now.strftime("%d.%m.%Y %H:%M:%S")
 	print("Current TIME: " + current_time)
+	logger.write("Current TIME: " + current_time + "\n")
 	print("GATEWAY IP  : " + argData["gateway_ip"])
+	logger.write("GATEWAY IP  : " + argData["gateway_ip"] + "\n")
 	print("GATEWAY UID : " + argData["UID"])
+	logger.write("GATEWAY UID : " + argData["UID"] + "\n")
 	if argData['ThingSpeakLogger'] == "True":
 		print("ThingSpeak  : True")
+		logger.write("ThingSpeak  : True" + "\n")
 	else:
 		print("ThingSpeak  : False")
+		logger.write("ThingSpeak  : False" + "\n")
 	if argData['XLSLogger'] == "True":
 		print("XLS logger  : True")
+		logger.write("XLS logger  : True" + "\n")
 	else:
 		print("XLS logger  : False")
+		logger.write("XLS logger  : False" + "\n")
 	for i in range(79):
 		print("-", end="")
+		logger.write("-")
 	print("-")
+	logger.write("-" + "\n")
 
 	# read data from SALUS GATEWAY
 	for i in range(int(listUID['max_climate_devices'])):
@@ -71,12 +85,15 @@ async def main():
 			await gateway.connect()
 		except IT600ConnectionError:
 			print("Connection error: check if you have specified gateway's IP address correctly.", file=sys.stderr)
+			logger.write("Connection error: check if you have specified gateway's IP address correctly." + "\n")
 			sys.exit(1)
 		except IT600AuthenticationError:
 			print("Authentication error: check if you have specified gateway's EUID correctly.", file=sys.stderr)
+			logger.write("Authentication error: check if you have specified gateway's EUID correctly." + "\n")
 			sys.exit(2)
 		except ConnectionAbortedError:
 			print("Software v hostitelském počítači ukončil vytvořené připojení.", file=sys.stderr)
+			logger.write("Software v hostitelském počítači ukončil vytvořené připojení." + "\n")
 			sys.exit(3)
 
 		await gateway.add_climate_update_callback(my_climate_callback)
@@ -122,6 +139,7 @@ async def main():
 				ThingSpeakHumidity.send()
 			if argData['XLSLogger'] == "True":
 				XLSaddData(temperature, humidity)
+			logger.close()
 
 
 if __name__ == "__main__":
